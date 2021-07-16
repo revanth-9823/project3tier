fd4e6c0ad95f223abab6d35306e36671cd36e2b0
revanth-9823 <85180789+revanth-9823@users.noreply.github.com>
2021-07-16 14:28:42 +0530

    Update main.tf
    creds n provider

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-provider azurerm {
-   features {}
-  
-  subscription_id = "596edce6-97a9-44ad-acf7-e4848b14c029"
-  client_id       = "ebf1239d-b661-4d52-b98c-0392d4976be6"
-  client_secret   = "q_l-imSY..P__-krE~m5Tw700G1i~zv-2f"
-  tenant_id       = "26fbe7de-7c9b-4775-aab3-59ebabc3763a"
-}
-
-
 
 module testvpc {
   source = "./modules/vnet"
